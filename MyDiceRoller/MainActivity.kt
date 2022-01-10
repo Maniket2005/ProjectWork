79f8b8adaf6562d82a47f7639c4158806238dc47
Maniket2005 <96408498+Maniket2005@users.noreply.github.com>
2022-01-10 22:26:15 +0530

    Add files via upload

---
+++ MyDiceRoller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        rollButton.setOnClickListener {
+
+            var c: Int = (1..6).random()
+
+            when (c) {
+                1 -> imageView.setImageResource(R.drawable.dice_1)
+                2 -> imageView.setImageResource(R.drawable.dice_2)
+                3 -> imageView.setImageResource(R.drawable.dice_3)
+                4 -> imageView.setImageResource(R.drawable.dice_4)
+                5 -> imageView.setImageResource(R.drawable.dice_5)
+                6 -> imageView.setImageResource(R.drawable.dice_6)
+            }
+            textView.text = c.toString()
+
+        }
+    }
+}
